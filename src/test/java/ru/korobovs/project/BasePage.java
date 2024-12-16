d3923b8272d8a82bdd083796250aadffce3164eb
KorobovS <korobovs33@yandex.ru>
2024-12-16 19:23:07 +0300

    add wait's BasePage

--- src/test/java/ru/korobovs/project/BasePage.java
+++ src/test/java/ru/korobovs/project/BasePage.java
@@ -2,10 +2,17 @@ package ru.korobovs.project;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public abstract class BasePage {
 
-    public WebDriver driver;
+    private WebDriver driver;
+
+    private WebDriverWait wait2;
+    private WebDriverWait wait5;
+    private WebDriverWait wait10;
 
     protected WebDriver getDriver() {
         if (driver != null) {
@@ -19,5 +26,29 @@ public abstract class BasePage {
         this.driver = driver;
 
         PageFactory.initElements(driver, this);
+    }
+
+    protected WebDriverWait getWait2() {
+        if (wait2 == null) {
+            wait2 = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        }
+
+        return wait2;
+    }
+
+    protected WebDriverWait getWait5() {
+        if (wait5 == null) {
+            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        }
+
+        return wait5;
+    }
+
+    protected WebDriverWait getWait10() {
+        if (wait10 == null) {
+            wait10 = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+
+        return wait10;
     }
 }
