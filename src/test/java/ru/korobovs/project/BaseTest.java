7461f49679780d1b2141b592f89157a3cff1f6d1
KorobovS <korobovs33@yandex.ru>
2024-12-16 19:35:49 +0300

    add wait's BaseTest

--- src/test/java/ru/korobovs/project/BaseTest.java
+++ src/test/java/ru/korobovs/project/BaseTest.java
@@ -1,15 +1,21 @@
 package ru.korobovs.project;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.lang.reflect.Method;
+import java.time.Duration;
 
 public abstract class BaseTest {
 
     private WebDriver driver;
+
+    private WebDriverWait wait2;
+    private WebDriverWait wait5;
+    private WebDriverWait wait10;
 
     protected WebDriver getDriver() {
         if (driver != null) {
@@ -55,5 +61,29 @@ public abstract class BaseTest {
 
             ProjectUtils.log("Браузер закрыт");
         }
+    }
+
+    protected WebDriverWait getWait2() {
+        if (wait2 == null) {
+            wait2 = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        }
+
+        return wait2;
+    }
+
+    protected WebDriverWait getWait5() {
+        if (wait5 == null) {
+            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        }
+
+        return wait5;
+    }
+
+    protected WebDriverWait getWait10() {
+        if (wait10 == null) {
+            wait10 = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        }
+
+        return wait10;
     }
 }
