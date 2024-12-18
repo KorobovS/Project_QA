b0e1d37785c357cda630f8a078179701fa0e4d6a
KorobovS <108029978+KorobovS@users.noreply.github.com>
2024-12-18 11:34:34 +0300

    Update ProjectUtils.java

--- src/test/java/ru/korobovs/project/ProjectUtils.java
+++ src/test/java/ru/korobovs/project/ProjectUtils.java
@@ -3,6 +3,7 @@ package ru.korobovs.project;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.PageLoadStrategy;
 
 import java.time.Duration;
 
