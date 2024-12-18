0fa06ca15b38aa22c0f52c32c9bdaa4c26d6bc05
KorobovS <korobovs33@yandex.ru>
2024-12-18 21:08:26 +0300

    add

--- src/test/java/ru/korobovs/project/ProjectUtils.java
+++ src/test/java/ru/korobovs/project/ProjectUtils.java
@@ -3,16 +3,68 @@ package ru.korobovs.project;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.PageLoadStrategy;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
+import java.util.Properties;
 
 public class ProjectUtils {
 
+    private static final String PREFIX_PROP = "local.";
+    private static final String ENV_CHROME_OPTIONS = "CHROME_OPTIONS";
+    private static final String PROP_CHROME_OPTIONS = PREFIX_PROP + ENV_CHROME_OPTIONS.toLowerCase();
+    private static final String ENV_APP_OPTIONS = "APP_OPTIONS";
+
+    private static Properties properties;
+
+    private static void initProperties() {
+        if (properties == null) {
+            properties = new Properties();
+            if (isServerRun()) {
+                properties.setProperty(PROP_CHROME_OPTIONS, System.getenv(ENV_CHROME_OPTIONS));
+
+                if (System.getenv(ENV_APP_OPTIONS) != null) {
+                    for (String option : System.getenv(ENV_APP_OPTIONS).split(";")) {
+                        String[] optionArr = option.split("=");
+                        properties.setProperty(PREFIX_PROP + optionArr[0], optionArr[1]);
+                    }
+                }
+            } else {
+                try {
+                    InputStream inputStream = ProjectUtils.class.getClassLoader().getResourceAsStream("local.properties");
+                    if (inputStream == null) {
+                        log("The \u001B[31mlocal.properties\u001B[0m file not found in src/test/resources/ directory.");
+                        log("You need to create it from local.properties.TEMPLATE file.");
+                        System.exit(1);
+                    }
+                    properties.load(inputStream);
+                } catch (IOException ignore) {
+                }
+            }
+        }
+    }
+
+    static boolean isServerRun() {
+        return System.getenv("CI_RUN") != null;
+    }
+
+    static final ChromeOptions chromeOptions;
+
+    static {
+        initProperties();
+
+        chromeOptions = new ChromeOptions();
+        String options = properties.getProperty(PROP_CHROME_OPTIONS);
+        if (options != null) {
+            for (String argument : options.split(";")) {
+                chromeOptions.addArguments(argument);
+            }
+        }
+    }
+
     static WebDriver createDriver() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        WebDriver driver = new ChromeDriver(ProjectUtils.chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
         return driver;
