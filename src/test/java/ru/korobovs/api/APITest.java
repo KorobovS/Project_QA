a67787d39724db0f3c4455bd67487dbcc40fe7c8
KorobovS <korobovs33@yandex.ru>
2024-12-19 21:09:03 +0300

    refactoring API

--- src/test/java/ru/korobovs/api/APITest.java
+++ src/test/java/ru/korobovs/api/APITest.java
@@ -4,6 +4,7 @@ import com.google.common.net.HttpHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.Test;
 import ru.korobovs.project.ProjectUtils;
 
@@ -20,7 +21,7 @@ public class APITest {
     private static final Logger log = LoggerFactory.getLogger(APITest.class);
 
     @Test
-    public void testHttp(Method method) throws URISyntaxException, IOException, InterruptedException {
+    public void testHttp(Method method, ITestResult testResult) throws URISyntaxException, IOException, InterruptedException {
         ProjectUtils.logf("Запускается %s.%s", this.getClass().getName(), method.getName());
 
         HttpClient httpClient = HttpClient.newHttpClient();
@@ -36,10 +37,12 @@ public class APITest {
 
         String body = httpResponse.body();
         Assert.assertNotNull(body);
+
+        ProjectUtils.logf("Время выполнения %.3f sec", (testResult.getEndMillis() - testResult.getStartMillis()) / 1000.0);
     }
 
     @Test
-    public void testSWapi(Method method) throws URISyntaxException, IOException, InterruptedException {
+    public void testSWapi(Method method, ITestResult testResult) throws URISyntaxException, IOException, InterruptedException {
         ProjectUtils.logf("Запускается %s.%s", this.getClass().getName(), method.getName());
 
         HttpClient httpClient = HttpClient.newHttpClient();
@@ -55,5 +58,7 @@ public class APITest {
 
         String body = httpResponse.body();
         Assert.assertTrue(body.startsWith("{\"message\":\"ok\""));
+
+        ProjectUtils.logf("Время выполнения %.3f sec", (testResult.getEndMillis() - testResult.getStartMillis()) / 1000.0);
     }
 }
