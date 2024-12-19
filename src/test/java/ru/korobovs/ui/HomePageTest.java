ac834fadaa13bc3c888a3c37673a66abd91b521e
KorobovS <korobovs33@yandex.ru>
2024-12-19 14:06:55 +0300

    new test

---
+++ src/test/java/ru/korobovs/ui/HomePageTest.java
@@ -0,0 +1,27 @@
+package ru.korobovs.ui;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.korobovs.project.BaseTest;
+import ru.korobovs.page.HomePage;
+
+public class HomePageTest extends BaseTest {
+
+    @Test
+    public void testTitle() {
+
+        String title = new HomePage(getDriver())
+                .getTitle();
+
+        Assert.assertEquals(title, "Switch to DuckDuckGo. It’s private and free!");
+    }
+
+    @Test
+    public void testUrlAboutPage() {
+        String title = new HomePage(getDriver())
+                .clickLogo()
+                .getTitle();
+
+        Assert.assertEquals(title, "Your personal data is\nnobody's business.");
+    }
+}
