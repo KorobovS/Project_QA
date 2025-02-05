29f49fdb56c25a446f577aa3d81569add3f5a33e
KorobovS <korobovs33@yandex.ru>
2025-02-05 17:22:22 +0300

    changed chromeOptions ProjectUtils

--- src/test/java/ru/korobovs/project/ProjectUtils.java
+++ src/test/java/ru/korobovs/project/ProjectUtils.java
@@ -15,10 +15,10 @@ public class ProjectUtils {
 
         chromeOptions.addArguments("--lang=en");
         chromeOptions.addArguments("--window-size=1920,1080");
-        chromeOptions.addArguments("--headless=new");
-//        chromeOptions.addArguments("--disable-dev-shm-usage");
-//        chromeOptions.addArguments("--disable-gpu;--no-sandbox");
-//        chromeOptions.addArguments("--remote-allow-origins=*");
+//        chromeOptions.addArguments("--headless=new");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--disable-gpu;--no-sandbox");
+        chromeOptions.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
 //        WebDriver driver = new FirefoxDriver();
